d05ddc7650133dad213a1736ca7d1a057861a33a
trungnguyenduc <trungnguyenduc>
2023-06-07 17:14:22 +0700

    update payment methods

--- Model/PaymentMethod.php
+++ Model/PaymentMethod.php
@@ -2,6 +2,44 @@
 
 namespace Wechannel\Custom\Model;
 
-class PaymentMethod extends Method implements \Wechannel\Custom\Api\Data\PaymentMethodInterface
+class PaymentMethod implements \Wechannel\Custom\Api\Data\PaymentMethodInterface
 {
+    protected $code;
+    protected $name;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
